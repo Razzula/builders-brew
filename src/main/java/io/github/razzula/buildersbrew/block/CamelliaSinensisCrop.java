ac99e14e5054ff85f73db76a958a3bd4e1a7c2cd
Razzula <jack02gillespie@gmail.com>
2025-03-18 22:47:48 +0000

    Update tea crops

--- src/main/java/io/github/razzula/buildersbrew/block/CamelliaSinensisCrop.java
+++ src/main/java/io/github/razzula/buildersbrew/block/CamelliaSinensisCrop.java
@@ -2,6 +2,8 @@ package io.github.razzula.buildersbrew.block;
 
 import net.minecraft.world.item.Item;
 import net.minecraft.world.level.block.CropBlock;
+import net.minecraft.network.chat.MutableComponent;
+import net.minecraft.network.chat.Component;
 
 import io.github.razzula.buildersbrew.registry.ModItems;
 
@@ -13,5 +15,23 @@ public class CamelliaSinensisCrop extends CropBlock {
     @Override
     protected Item getBaseSeedId() {
         return ModItems.CAMELLIA_SINENSIS_SEEDS.get();
+    }
+
+    @Override
+    public MutableComponent getName() {
+        int age = this.defaultBlockState().getValue(this.getAgeProperty());
+
+        if (age == 0) {
+            return Component.translatable("block.buildersbrew.camellia_sinensis_crop.seed");
+        } else if (age <= 2) {
+            return Component.translatable("block.buildersbrew.camellia_sinensis_crop.sprout");
+        } else {
+            return Component.translatable("block.buildersbrew.camellia_sinensis_crop.bush");
+        }
+    }
+
+    @Override
+    public int getMaxAge() {
+        return 7; // limits crop growth to 8 stages (0-7)
     }
 }
